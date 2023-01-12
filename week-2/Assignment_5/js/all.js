ad8c9c1ceb0071d6b5cec3eb604af27b5890ab7a
wanhsuanlin <energy2429@gmail.com>
2023-01-12 12:23:26 +0800

    Use array.push methods

--- week-2/Assignment_5/js/all.js
+++ week-2/Assignment_5/js/all.js
@@ -3,7 +3,7 @@ function twoSum(nums, target) {
   for(let i = 0; i < nums.length; i++) {
     for(let j = i + 1; j < nums.length; j++) {
       if(nums[i] + nums[j] === target) {
-        index = [i, j];
+        index.push(i, j);
       }
     }
   }
