bf222744e08e3de24f4744bfa8e8e2df266c1f3f
wanhsuanlin <energy2429@gmail.com>
2023-01-12 12:13:51 +0800

    Add Assignment_5 answer

--- week-2/Assignment_5/js/all.js
+++ week-2/Assignment_5/js/all.js
@@ -1,19 +1,13 @@
 function twoSum(nums, target) {
-  
+  let index = [];
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+      if(nums[i] + nums[j] === target) {
+        index = [i, j];
+      }
+    }
+  }
+  return index;
 }
 
-
-/*
-For example:
-    twoSum([2, 7, 11, 15], 9); 
-Should returns:
-    [0, 1] 
-Because:
-    nums[0] + nums[1] is 9
-*/
-
-/*
-給定一個整數陣列，回傳兩個數字的索引，以便它們加起來達到一個特定的目標：
-1. 您可能假設每個輸入將只有一個解決方案
-2. 並且您可能不會兩次使用相同的元素
-*/
+console.log(twoSum([2, 7, 11, 15], 9));
