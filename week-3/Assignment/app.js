59bd74bda0930224c8a75578840c244f07830b1d
wanhsuanlin <energy2429@gmail.com>
2023-01-21 16:55:25 +0800

    Add fetch method to sum.html

--- week-3/Assignment/app.js
+++ week-3/Assignment/app.js
@@ -3,14 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use("/static", express.static("public"));
+app.use("/", express.static(__dirname + "/public"));
 
 app.set("view engine", "pug");
 
 const mainRoutes = require("./routes");
 const getDataRoutes = require("./routes/getData");
+const sumDataRoutes = require("./routes/sumData");
 
 app.use(mainRoutes);
 app.use("/getData", getDataRoutes);
+app.use("/sumData", sumDataRoutes);
 
 app.use((req, res, next) => {
   const err = new Error("Not Found");
