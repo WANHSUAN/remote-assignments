b2fcfd9ec3215c3f71661819d56bf259aec7f884
wanhsuanlin <energy2429@gmail.com>
2023-01-23 12:00:54 +0800

    Add replaceAll() to replace %20 to space

--- week-3/Assignment/public/js/all.js
+++ week-3/Assignment/public/js/all.js
@@ -2,7 +2,8 @@
 function getCookie() {
     let cookies = document.cookie;
     let cookiesNumber = cookies.split("=");
-    let cookiesAnswer = cookiesNumber[1];
+    let cookiesAnswer = cookiesNumber[1].replaceAll("%20", " ");
+    console.log(cookiesAnswer);
     const answer = document.getElementById("answer");
     answer.innerHTML = `
     <h1>Answer: ${cookiesAnswer}</h1>
